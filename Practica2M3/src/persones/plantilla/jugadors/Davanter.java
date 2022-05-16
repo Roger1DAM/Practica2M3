a4afa52e0260bfe201b3d7dc65fe77bcc9c22b4b
Roger1DAM <94872585+Roger1DAM@users.noreply.github.com>
2022-05-16 22:31:20 +0200

    Add files via upload

---
+++ Practica2M3/src/persones/plantilla/jugadors/Davanter.java
@@ -0,0 +1,69 @@
+package persones.plantilla.jugadors;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import validacions.Correu;
+import validacions.Dni;
+import validacions.Telefon;
+
+public class Davanter extends Jugador implements Serializable{
+    private int gols;
+    private int balonsRecuperats;
+    private static int incentiuGols = 500;
+    private static int incentiuBR = 100;
+
+    @Override
+    public void calcularSouIncentivat() {
+        setSouIncentivat(getSouBase() + (gols * incentiuGols) + (balonsRecuperats * incentiuBR));
+        
+    }
+
+    public Davanter(Dni dni, String nom, String cognom1, String cognom2, LocalDate data_naix, Telefon telefon,
+            Correu email, String nss, int souBase, boolean esTitular, int gols,
+            int balonsRecuperats) {
+        super(dni, nom, cognom1, cognom2, data_naix, telefon, email, nss, souBase, esTitular);
+        this.gols = gols;
+        this.balonsRecuperats = balonsRecuperats;
+        calcularSouIncentivat();
+    }
+
+    public Davanter() {
+
+    }
+
+    public int getGols() {
+        return gols;
+    }
+
+    public void setGols(int gols) {
+        this.gols = gols;
+    }
+
+    public int getBalonsRecuperats() {
+        return balonsRecuperats;
+    }
+
+    public void setBalonsRecuperats(int balonsRecuperats) {
+        this.balonsRecuperats = balonsRecuperats;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nBalons Recuperats: " + balonsRecuperats + "\nGols: " + gols;
+    }
+
+    public Davanter altaDavanter() {
+        Scanner kb = new Scanner(System.in);
+        altaJugador();
+        System.out.print("Escriu els gols anotats: ");
+        setGols(kb.nextInt());
+
+        System.out.print("Escriu els balons recuperats: ");
+        setBalonsRecuperats(kb.nextInt());
+        calcularSouIncentivat(); 
+        return this;
+    }
+    
+}
---
+++ Practica2M3/src/persones/plantilla/jugadors/Porter.java
@@ -0,0 +1,75 @@
+package persones.plantilla.jugadors;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import validacions.Correu;
+import validacions.Dni;
+import validacions.Telefon;
+
+public class Porter extends Jugador implements Serializable{
+    private int golsAturats;
+    private int golsRebuts;
+    private static int incentiuGA = 150;
+
+
+    public int getGolsAturats() {
+        return golsAturats;
+    }
+
+    public void setGolsAturats(int golsAturats) {
+        this.golsAturats = golsAturats;
+    }
+
+    public int getGolsRebuts() {
+        return golsRebuts;
+    }
+
+    public void setGolsRebuts(int golsRebuts) {
+        this.golsRebuts = golsRebuts;
+    }
+
+    public static int getIncentiuGA() {
+        return incentiuGA;
+    }
+
+    public static void setIncentiuGA(int incentiuGA) {
+        Porter.incentiuGA = incentiuGA;
+    }
+
+    @Override
+    public void calcularSouIncentivat() {
+        setSouIncentivat(getSouBase() + (getGolsAturats() * incentiuGA));;
+    }
+
+    public Porter(Dni dni, String nom, String cognom1, String cognom2, LocalDate data_naix, Telefon telefon,
+            Correu email, String nss, int souBase, boolean esTitular, int golsAturats,
+            int golsRebuts) {
+        super(dni, nom, cognom1, cognom2, data_naix, telefon, email, nss, souBase, esTitular);
+        this.golsAturats = golsAturats;
+        this.golsRebuts = golsRebuts;
+        calcularSouIncentivat();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nGols Aturats: " + golsAturats + "\nGols Rebuts: " + golsRebuts;
+    }
+
+    public Porter() {
+        
+    }
+
+    public Porter altaPorter() {
+        Scanner kb = new Scanner(System.in);
+        altaJugador();
+        System.out.print("Escriu els gols aturats: ");
+        setGolsAturats(kb.nextInt());
+        System.out.print("Escriu els gols rebuts: ");
+        setGolsRebuts(kb.nextInt());
+        return this;
+    }
+}
+
+
