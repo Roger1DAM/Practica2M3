4d00edae6eae582a108ab002fcc7b876e5dd863d
Roger1DAM <94872585+Roger1DAM@users.noreply.github.com>
2022-05-30 18:54:13 +0200

    Delete src directory

--- src/ClubFutbolApp.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class ClubFutbolApp {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Club c = new Club("Tarrega C.F", "123456789", "C/ Tal de pasqual", "121212121", "tarregaCF@gmail.com", "www.tarregaCF.com");
-        c.gestionarClub();
-    }
-}
